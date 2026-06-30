7348709c4e8e48d25f55e871b9e75fb5a315bd79
CyberBASSLord-666 <sicp1989@gmail.com>
2026-06-29 20:47:41 -0600

    Apply rustfmt import ordering

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@
 
 use axum::{routing::get, Router};
 use termux_mcp_server::{
-    config::{AuthPosture, AppConfig, validate_runtime_auth_posture},
+    config::{AppConfig, AuthPosture, validate_runtime_auth_posture},
     tools::FileSystemTools,
 };
 use tokio::signal;
